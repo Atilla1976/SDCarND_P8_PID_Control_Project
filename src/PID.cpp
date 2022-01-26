04e05c729f4c95dd4f2e42e6eeb9ca792cbb5372
Atilla Özdemir <81968553+Atilla1976@users.noreply.github.com>
2022-01-26 10:13:30 +0100

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,22 +9,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+  
+  this->Kp_ = Kp_;
+  this->Ki_ = Ki_;
+  this->Kd_ = Kd_;
+  
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
 
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+  
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+  
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  
+  return -Kp_ * p_error - Ki_ * i_error - Kd * d_error
 }
