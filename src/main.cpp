a93e9047afcfac3dd0a2f51da38791507ca5b5f8
Atilla Özdemir <81968553+Atilla1976@users.noreply.github.com>
2022-01-26 15:40:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ int main() {
   // pid.Init(0.08, 0.000, 1.7); ... case 13: coefficient kd higher than case 12;  ... again slightly better in lane line area but not really good
   // pid.Init(0.08, 0.004, 1.7); ... case 14: next coefficient -> ki; first try ki = 0.004 ... car could not keep the lane 
   // pid.Init(0.08, 0.001, 1.7); ... case 13: coefficient kd higher than case 12;  ... car could not keep the lane but with strong oszillation
-  pid.Init(0.08, 0.0001, 1.7);
+  pid.Init(0.08, 0.0005, 1.7);
   
  
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
