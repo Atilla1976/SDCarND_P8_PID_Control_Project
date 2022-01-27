bd93e116b209ddf1ba5d692e90f4aa64a2946293
Atilla Özdemir <81968553+Atilla1976@users.noreply.github.com>
2022-01-27 15:19:26 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -86,8 +86,9 @@ int main() {
            *   Maybe use another PID controller to control the speed!
            */
           pid.UpdateError(cte);
-          
           steer_value = pid.TotalError();
+          if(steer_value > 1.0) steer_value = 1.0;
+          if(steer_value < -1.0) steer_value = -1.0;
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
