cd81cf28780972c554e94afe3b253b04cabbfba3
Atilla Özdemir <81968553+Atilla1976@users.noreply.github.com>
2022-01-27 16:54:01 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -34,30 +34,7 @@ int main() {
   uWS::Hub h;
 
   PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
-  // pid.Init(0.2, 0.000, 0.0); .... case 1: coefficient kp first try 0,2, ki and kd = 0; best case until now
-  // pid.Init(0.5, 0.000, 0.0); .... case 2: coefficient kp higher; ... to much oszillation
-  // pid.Init(1.0, 0.000, 0.0); .... case 3: coefficient kp even higher; ... as expected even worse oszillation
-  // pid.Init(0.1, 0.000, 0.0); .... case 4: coefficient kp lower than case 1; ... better result
-  // pid.Init(0.05, 0.000, 0.0); ... case 5: coefficient kp lower than case 4; ... better result
-  // pid.Init(0.01, 0.000, 0.0); ... case 6: coefficient kp lower than case 5; ... worse result
-  // pid.Init(0.03, 0.000, 0.0); ... case 7: coefficient kp higher than case 6; ... farthest distance traveled so far, but too soft correction
-  // pid.Init(0.08, 0.000, 0.0); ... case 8: coefficient kp higher than case 5; ... kp range between 0.5 an 1.0 gets the best results
-  // pid.Init(0.08, 0.000, 3.0); ... case 9: next coefficient -> kd; first try kd = 3.0 ... car could pass the complete track but with to much oszillation
-  // pid.Init(0.08, 0.000, 2.0); ... case 10: coefficient kd lower than case 9 .... better result
-  // pid.Init(0.08, 0.000, 1.0); ... case 11: coefficient kd lower than case 10 ... partly dangerously close the the lane lines
-  // pid.Init(0.08, 0.000, 1.5); ... case 12: coefficient kd higher than case 11;  ... slightly better in lane line area
-  // pid.Init(0.08, 0.000, 1.7); ... case 13: coefficient kd higher than case 12;  ... again slightly better in lane line area but not really good
-  // pid.Init(0.08, 0.004, 1.7); ... case 14: next coefficient -> ki; first try ki = 0.004 ... car could not keep the lane 
-  // pid.Init(0.08, 0.001, 1.7); ... case 15: coefficient ki lower than case 14;  ... car could not keep the lane but with strong oszillation
-  // pid.Init(0.09, 0.001, 1.7); ... case 15: coefficient ki lower than case 14;  ... car could not keep the lane but with strong oszillation
-  // pid.Init(0.12, 0.0005, 1.8); // .. case 16: coefficient ki lower than case 15;  ...best result so far
-  // pid.Init(0.12, 0.0007, 1.9);
-  // pid.Init(0.13, 0.00095, 1.9);  car inside lane lines
-  // pid.Init(0.13, 0.00095, 1.7); car inside lane lines
-  // pid.Init(0.2, 0.00095, 1.68);
+  
   pid.Init(0.12, 0.00007, 1.62);
   
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
